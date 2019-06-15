6ef4f8e955a4f3ed75bfdee8e029fa6ca21b1f30
Makarovks320 <kirill.makarov@tafs.pro>
2019-06-16 01:12:01 +0300

    + параметры по умолчанию и для возврата значений функций

--- js/setup.js
+++ js/setup.js
@@ -1,70 +1,81 @@
 'use strict';
 
-var NAMES = ['Иван',
+var NAMES = [
+  'Иван',
   'Хуан Себастьян',
   'Мария',
   'Кристоф',
   'Виктор',
   'Юлия',
   'Люпита',
-  'Вашингтон'];
-var SURNAMES = ['да Марья',
+  'Вашингтон'
+];
+var SURNAMES = [
+  'да Марья',
   'Верон',
   'Мирабелла',
   'Вальц',
   'Онопко',
   'Топольницкая',
   'Нионго',
-  'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)',
+  'Ирвинг'
+];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
   'rgb(146, 100, 161)',
   'rgb(56, 159, 117)',
   'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black',
+  'rgb(0, 0, 0)'
+];
+var EYES_COLORS = [
+  'black',
   'red',
   'blue',
   'yellow',
-  'green'];
+  'green'
+];
 var wizards = [];
 var wizardElements = [];
 
 document.querySelector('.setup').classList.remove('hidden');
 
-getRandomWizards(4); // генерирует массив из 4-х волшебников
+getRandomWizards(wizards, 4); // генерирует массив из волшебников
 
-createWizardElements(); // создает DOM-элементы с данными случайно сгенерированных волшебников
+createWizardElements(wizards); // создает DOM-элементы с данными случайно сгенерированных волшебников
 
-displayWizards(); // показывает их в блоке .setup-similar-list
+displayWizards(wizards); // показывает их в блоке .setup-similar-list
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 function getRandomInteger(max, min) {
+  if (min === undefined) {
+    min = 0;
+  }
   return Math.round(Math.random() * (max - min) + min);
 }
-function getRandomWizards(wizardsNumber) {
+function getRandomWizards(arrayName, wizardsNumber) {
   for (var i = 0; i < wizardsNumber; i++) {
     var wizard = {};
-    wizard.name = NAMES[getRandomInteger(NAMES.length - 1, 0)] + ' ' + SURNAMES[getRandomInteger(SURNAMES.length - 1, 0)];
-    wizard.coatColor = COAT_COLORS[getRandomInteger(COAT_COLORS.length - 1, 0)];
-    wizard.eyesColor = EYES_COLORS[getRandomInteger(EYES_COLORS.length - 1, 0)];
-    wizards[i] = wizard;
+    wizard.name = NAMES[getRandomInteger(NAMES.length - 1)] + ' ' + SURNAMES[getRandomInteger(SURNAMES.length - 1)];
+    wizard.coatColor = COAT_COLORS[getRandomInteger(COAT_COLORS.length - 1)];
+    wizard.eyesColor = EYES_COLORS[getRandomInteger(EYES_COLORS.length - 1)];
+    arrayName[i] = wizard;
   }
-  return wizards;
+  return arrayName;
 }
-function createWizardElements() {
+function createWizardElements(someArray) {
   var template = document.querySelector('#similar-wizard-template');
-  for (var i = 0; i < wizards.length; i++) {
+  for (var i = 0; i < someArray.length; i++) {
     var wizardElement = template.content.querySelector('.setup-similar-item').cloneNode(true);
-    wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+    wizardElement.querySelector('.setup-similar-label').textContent = someArray[i].name;
+    wizardElement.querySelector('.wizard-coat').style.fill = someArray[i].coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = someArray[i].eyesColor;
     wizardElements[i] = wizardElement;
   }
 }
-function displayWizards() {
-  for (var i = 0; i < wizards.length; i++) {
+function displayWizards(someArray) {
+  for (var i = 0; i < someArray.length; i++) {
     document.querySelector('.setup-similar-list').appendChild(wizardElements[i]);
   }
 }
